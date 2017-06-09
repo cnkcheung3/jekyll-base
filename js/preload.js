4e8bec064055faa5e4c7bb8b2ed42bb0012111eb
Ken Cheung <ken.cheung@zensis.com>
2017-06-09 10:05:18 +0800

    + gem file

--- js/preload.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener(
-    'load',
-    function load()
-    {
-        window.removeEventListener('load', load, false);
-        document.body.classList.remove('preload');
-        // document.getElementById("body").style.display = "block";
-    },
-    false);
